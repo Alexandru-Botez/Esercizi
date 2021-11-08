3237df76fb5127438e8971d58a6be5d711c14fdc
jlecs <90066994+Alexandru-Botez@users.noreply.github.com>
2021-11-08 23:38:11 +0100

    Update main.cpp

--- while exercise/legge na sequenza di numeri i nteri e ne ricerca il valore minimo/main.cpp
+++ while exercise/legge na sequenza di numeri i nteri e ne ricerca il valore minimo/main.cpp
@@ -1,23 +1,16 @@
 #include <iostream>
 using namespace std;
-
-
-int main()
-{	
-	int num = 1, min = 256*256;
+void ciao()
+{
+	int num = 1 , min = 0;
 
 	while (num > 0)
 	{
 		cout << "inserisci un numero: ";
 		cin >> num;
-		if (num > 0 && num < min)
-		{
+
+		if (num > 0 && (min == 0 || num < min))
 			min = num;
-		}
 	}
-
-	cout << "il numero minimo e : " << min;
-
-
-	return 0;
+	cout << "il minore e " << min;
 }
