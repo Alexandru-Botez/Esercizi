c6792563b9a949cbee09f825c65791a6a4a5899c
jlecs <90066994+Alexandru-Botez@users.noreply.github.com>
2021-11-30 23:41:10 +0100

    Update main.cpp

--- for exercises/stampa un triangolo di asterischi dato un Numero positivo/main.cpp
+++ for exercises/stampa un triangolo di asterischi dato un Numero positivo/main.cpp
@@ -9,13 +9,13 @@ int main()
     int spazio;
   
   
-    // Chiede un numero positivo
+    // Chiede un numero positivo e disparo
     do
     {
         cout << "Inserisci il numero di asterischi: ";
         cin >> N;
 
-    } while (0 > N);
+    } while (0 > N && N % 2 == 0);
     
   // Riga di asterischi dato un Numero
     for (i = 0; i < N; i++)
@@ -69,6 +69,41 @@ int main()
         for (i = 0; i < colonne * 2 - 1; i++)
         {
             cout << "* ";
+        }
+
+        cout << endl;
+    }
+    
+    // Rombo
+     for (colonne = 0; colonne <= (N + 1) / 2; colonne++)
+    {
+        for (spazio = 0; spazio <= N / 2 - colonne; spazio++)
+        {
+            cout << "  ";
+        }
+
+        for (i = 0; i < colonne * 2 - 1; i++)
+        {
+            if (i == 0 || i == colonne * 2 - 2)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+
+        cout << endl;
+    }
+    for (colonne = 1; colonne <= N / 2; colonne++)
+    {
+        for (spazio = 0; spazio < colonne; spazio++)
+        {
+            cout << "  ";
+        }
+        for (i = N - colonne ; i > colonne; i--)
+        {
+            if (i == colonne + 1 || i == N - colonne)
+                cout << "* ";
+            else
+                cout << "  ";
         }
 
         cout << endl;
